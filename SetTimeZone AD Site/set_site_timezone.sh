a5876c2c50c805c76c062e8229a730fcec636741
hunty1 <calum@Calum-iMac.local>
2014-10-15 15:57:28 +1100

    better logging and comments

--- SetTimeZone AD Site/set_site_timezone.sh
+++ SetTimeZone AD Site/set_site_timezone.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
-######################################################
-#                                                    #
-# Author:   Calum Hunter                             #
-# Date:     14-10-2014                               #
-# Version:  0.1                                      #
-# Purpose:  Set the timezone based upon AD Site ID   #
-#                                                    #
-######################################################
+#########################################################
+#                                                       #
+# Author:   Calum Hunter                                #
+# Date:     14-10-2014                                  #
+# Version:  0.2                                         #
+# Purpose:  Set the timezone based upon AD Site ID      #
+# Changes:  0.2 Better comments &log output to syslog   #
+#                                                       #
+#########################################################
 
-# First lets get our site code. scoop this up from /var
+# Location of our site and building information
 info="/var/location_details.txt"
 
-# Now parse this to grab just our 4 digit site code
+# Parse through our information file to pull out our site code
 site=`cat $info | cut -d " " -f 2 | cut -c 1-4`
 
-## Log some info to stdout about what we are doing.
+## This function logs our info to the system.log
 notify(){
-	echo "*** INFO ***    Site is outside of standard AEST/AEDST (Sydney) timezone!"
-	echo "*** INFO ***    Checking AD Site code to determine correct timezone"
-	echo "*** INFO ***    Found site code:- $site"
-	echo "*** INFO ***    Setting timezone to $timezone"
+	logger -s "*** INFO ***    Site is outside of standard AEST/AEDST (Sydney) timezone!"
+	logger -s "*** INFO ***    Checking AD Site code to determine correct timezone"
+	logger -s "*** INFO ***    Found site code:- $site"
+	logger -s "*** INFO ***    Setting timezone to $timezone"
 }
 
 ## Check to see if our site code matches any of these special sites. Set the timezone variable on match. 
-case $1 in
+case $site in
 	1921)
-		timezone="Australia/Lord_Howe"
-		notify;;
-	1028|8105|1371|1369|1432|4232|4233|5302|8491)
-		timezone="Australia/Adelaide"
-		notify;;
+		timezone="Australia/Lord_Howe" 			# Site 1921 is Lord Howe - set time zone appropriately 
+		notify;;								# Run our notify function for logging purposes
+	1028|8105|1371|1369|1432|4232|4233|5302|8491)  # These sites are all ACST ie. Adelaide, so set timezone to Adelaide
+		timezone="Australia/Adelaide"			
+		notify;;								# Run our notify function for logging purposes
 		*) 
-		timezone="Australia/Sydney"
-		echo "*** INFO ***    Nothing special"
-		echo "*** INFO ***    Setting timezone to $timezone";;
+		timezone="Australia/Sydney"				# For everyone else, we assume you are in Sydney, so set timezone and log some info
+		logger -s "*** INFO ***    AD Site $site"
+		logger -s "*** INFO ***    No special timezones found"
+		logger -s "*** INFO ***    Setting timezone to $timezone";;
 esac
 
 # Set the timezone here
