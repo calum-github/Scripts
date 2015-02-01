c738c0e6169464d0917e0eb75861fa9d45123478
hunty1 <calum@Calum-iMac.local>
2015-02-02 08:49:30 +1100

    new script

---
+++ Adobe CC makepkginfo/adobe_cc_installs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "*** Making pkginfo for Adobe CC 2014 ***"
+echo "*** Use this for the installs array ***"
+
+makepkginfo \
+-f /Applications/Adobe\ Acrobat\ XI\ Pro/Adobe\ Acrobat\ Pro.app \
+-f /Applications/Adobe\ After\ Effects\ CC\ 2014/Adobe\ After\ Effects\ CC\ 2014.app \
+-f /Applications/Adobe\ Audition\ CC\ 2014/Adobe\ Audition\ CC\ 2014.app \
+-f /Applications/Adobe\ Bridge\ CC/Adobe\ Bridge\ CC.app \
+-f /Applications/Adobe\ Dreamweaver\ CC\ 2014.1/Adobe\ Dreamweaver\ CC\ 2014.1.app \
+-f /Applications/Adobe\ Edge\ Animate\ CC\ 2014.1/Adobe\ Edge\ Animate\ CC\ 2014.1.app \
+-f /Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app \
+-f /Applications/Adobe\ Extension\ Manager\ CC/Adobe\ Extension\ Manager\ CC.app \
+-f /Applications/Adobe\ Extension\ Manager\ CS6/Adobe\ Extension\ Manager\ CS6.app \
+-f /Applications/Adobe\ Fireworks\ CS6/Adobe\ Fireworks\ CS6.app \
+-f /Applications/Adobe\ Flash\ Builder\ 4.7/Adobe\ Flash\ Builder\ 4.7.app \
+-f /Applications/Adobe\ Flash\ CC\ 2014/Adobe\ Flash\ CC\ 2014.app \
+-f /Applications/Adobe\ Illustrator\ CC\ 2014/Adobe\ Illustrator.app \
+-f /Applications/Adobe\ InDesign\ CC\ 2014/Adobe\ InDesign\ CC\ 2014.app \
+-f /Applications/Adobe\ Media\ Encoder\ CC\ 2014/Adobe\ Media\ Encoder\ CC\ 2014.app \
+-f /Applications/Adobe\ Muse\ CC\ 2014/Adobe\ Muse\ CC\ 2014.app \
+-f /Applications/Adobe\ Photoshop\ CC\ 2014/Adobe\ Photoshop\ CC\ 2014.app \
+-f /Applications/Adobe\ Prelude\ CC\ 2014/Adobe\ Prelude\ CC\ 2014.app \
+-f /Applications/Adobe\ Premiere\ Pro\ CC\ 2014/Adobe\ Premiere\ Pro\ CC\ 2014.app \
+-f /Applications/Adobe\ SpeedGrade\ CC\ 2014/Adobe\ SpeedGrade\ CC\ 2014.app 
+
+exit 0
