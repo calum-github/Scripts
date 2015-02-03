8b2f5df6c7b68d9c1e6896b1c2b7f0bc90cadda3
hunty1 <calum@Calum-iMac.local>
2015-02-03 17:55:41 +1100

    fix stuck yosemite boot issue
    this should be installed on the problem machine in /etc

---
+++ Fix stuck yosemite boot/rc.server.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+###########################################################################
+# Fix for AD Bound 10.10 machines with the stuck at 50% boot issue        #
+#                                                                         #
+# Date: 3-2-2015                                                          #
+#                                                                         #
+# Credit to:  Chris Hotte 2015 - No rights/blame reserved.                #
+#                                                                         #
+###########################################################################
+
+/usr/sbin/BootCacheControl jettison
+
